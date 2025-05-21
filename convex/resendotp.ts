382aad42e54e7b3f464495861bb66dc52e079c42
Ingo Wolf <me@ingowolf.au>
2025-05-21 20:04:51 +1000

    tweak email and fix code length

--- convex/resendotp.ts
+++ convex/resendotp.ts
@@ -20,14 +20,14 @@ export const ResendOTP = Email({
   maxAge: 60 * 15, // 15 minutes
   // This function can be asynchronous
   generateVerificationToken() {
-    return generateRandomString(8, alphabet("0-9"));
+    return generateRandomString(6, alphabet("0-9"));
   },
   async sendVerificationRequest({ identifier: email, provider, token }) {
     const resend = new ResendAPI(provider.apiKey);
     const { error } = await resend.emails.send({
-      from: "My App <onboarding@resend.dev>",
+      from: "Aranda Music and Arts Program <noreply@arandamusicprogram.org>",
       to: [email],
-      subject: `Sign in to My App`,
+      subject: `Sign in to AMA portal`,
       text: "Your code is " + token,
     });
 
