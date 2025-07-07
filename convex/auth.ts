b090ac8acf93dfb737852ed06d488f27d54678ee
Ingo Wolf <me@ingowolf.au>
2025-07-07 12:58:16 +1000

    add google for auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,7 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import { ResendOTP } from "./resendotp";
+import Google from "@auth/core/providers/google";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [ResendOTP],
+  providers: [ResendOTP, Google],
 });
