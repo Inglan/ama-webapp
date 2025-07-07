81e38d4d636ad2ade40ff39309f784740ba06181
Ingo Wolf <me@ingowolf.au>
2025-07-07 16:39:18 +1000

    add dashboard sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,32 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {}
+}>) {
+  return (
+    <SidebarProvider>
+      <main>
+        <Sidebar>
+          <SidebarHeader />
+          <SidebarContent>
+            <SidebarGroup />
+            <SidebarGroup />
+          </SidebarContent>
+          <SidebarFooter />
+        </Sidebar>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
