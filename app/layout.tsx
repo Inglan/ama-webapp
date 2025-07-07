a8846667139e96c4c8c5130989391ccdd42a4336
Ingo Wolf <me@ingowolf.au>
2025-07-07 16:14:42 +1000

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ConvexClientProvider } from "./ConvexClientProvider";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/navbar";
+import Link from "next/link";
 
 // Font configuration for optimal loading and performance
 const geistSans = Geist({
@@ -153,6 +154,20 @@ export default function RootLayout({
               {/* Main content area */}
               <main id="main-content" className="flex-1">
                 {children}
+                <footer className="prose prose-invert mx-auto p-3 text-center border-t border-dashed">
+                  This website was built by{" "}
+                  <Link href="https://ingo.au" target="_blank">
+                    Ingo Wolf
+                  </Link>{" "}
+                  with Next.js and Convex. It's also{" "}
+                  <Link
+                    href="https://github.com/Inglan/ama-webapp"
+                    target="_blank"
+                  >
+                    open source
+                  </Link>
+                  .
+                </footer>
               </main>
 
               {/* Global toast notifications */}
