31d619bb7db4b6012a2f2d5988c988015f48a90e
Ingo Wolf <me@ingowolf.au>
2025-07-07 13:18:54 +1000

    add hero text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="w-full h-screen relative flex justify-center items-center">
+      <div className="w-full h-screen relative flex justify-center items-center flex-col gap-3">
         <video
           src="/hero.mp4"
           className="w-full h-full object-cover opacity-50 absolute top-0 left-0 -z-10"
@@ -11,7 +11,10 @@ export default function Home() {
           muted
           loop
         ></video>
-        <h1 className="text-6xl text-center">Aranda Music and Arts Program</h1>
+        <h1 className="text-7xl text-center">Aranda Music and Arts Program</h1>
+        <h2 className="text-2xl text-center">
+          An after school music, drama and art program
+        </h2>
       </div>
     </>
   );
