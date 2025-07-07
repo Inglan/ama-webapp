5c1fba11d847a9cd78dc4a0e1ee6a9ed2e137f81
Ingo Wolf <me@ingowolf.au>
2025-07-07 14:38:12 +1000

    change background

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 sm:p-10">
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 sm:p-10">
       <div className="flex w-full sm:max-w-sm flex-col gap-6">
         <span className="flex items-center gap-2 self-center font-medium">
           Aranda Music and Arts Program
