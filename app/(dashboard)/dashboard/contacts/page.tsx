45bc0a2799b96dfefd95129dbc3a9cf3abc3878e
Ingo Wolf <me@ingowolf.au>
2025-07-09 21:33:37 +1000

    add add button

--- app/(dashboard)/dashboard/contacts/page.tsx
+++ app/(dashboard)/dashboard/contacts/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { columns, Contact } from "./columns";
 import { DataTable } from "./data-table";
+import { Plus } from "lucide-react";
 
 export default function ContactsPage() {
   const data: Contact[] = [
@@ -26,6 +28,11 @@ export default function ContactsPage() {
   return (
     <div className="max-w-2xl w-full mx-auto p-3 gap-3 flex flex-col">
       <h1 className="text-3xl">Emergency Contacts</h1>
+      <div className="flex flex-row justify-end">
+        <Button>
+          <Plus /> Add Contact
+        </Button>
+      </div>
       <DataTable data={data} columns={columns} />
     </div>
   );
