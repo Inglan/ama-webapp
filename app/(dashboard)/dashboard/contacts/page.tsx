8e6e961492ea1f3366cde12722305db576aaadcd
Ingo Wolf <me@ingowolf.au>
2025-07-09 21:20:24 +1000

    start emergency contants page

--- app/(dashboard)/dashboard/contacts/page.tsx
+++ app/(dashboard)/dashboard/contacts/page.tsx
@@ -1,8 +1,7 @@
 export default function ContactsPage() {
   return (
-    <div>
-      <h1>Contacts</h1>
-      {/* Add your content here */}
+    <div className="max-w-2xl w-full mx-auto border border-dashed p-3 rounded-xl">
+      <h1 className="text-3xl">Emergency Contacts</h1>
     </div>
   );
 }
