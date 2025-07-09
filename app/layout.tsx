0e2e7d8d79ae5fa10e4b8e4ac43261069b384617
Ingo Wolf <me@ingowolf.au>
2025-07-09 19:59:25 +1000

    use default styles to toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -151,17 +151,7 @@ export default function RootLayout({
               {children}
 
               {/* Global toast notifications */}
-              <Toaster
-                position="top-center"
-                toastOptions={{
-                  duration: 4000,
-                  style: {
-                    background: "hsl(var(--card))",
-                    color: "hsl(var(--card-foreground))",
-                    border: "1px solid hsl(var(--border))",
-                  },
-                }}
-              />
+              <Toaster />
             </ThemeProvider>
           </ConvexClientProvider>
         </body>
