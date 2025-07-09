62aa83b38087a23e3f43028563f096a38ba0d1c2
Ingo Wolf <me@ingowolf.au>
2025-07-09 21:46:52 +1000

    change to null because typescript

--- convex/userInfo.ts
+++ convex/userInfo.ts
@@ -5,9 +5,9 @@ export const get = query({
   args: {},
   handler: async (ctx) => {
     const userId = await getAuthUserId(ctx);
-    if (!userId) return false;
+    if (!userId) return null;
     const user = await ctx.db.get(userId);
-    if (!user) return false;
+    if (!user) return null;
     return { role: user.role, email: user.email, id: userId, name: user.name };
   },
 });
