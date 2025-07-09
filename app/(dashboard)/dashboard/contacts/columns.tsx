35a1d95f834895b7aba9d7cba3f9074ed04cab35
Ingo Wolf <me@ingowolf.au>
2025-07-09 21:32:12 +1000

    add actions

--- app/(dashboard)/dashboard/contacts/columns.tsx
+++ app/(dashboard)/dashboard/contacts/columns.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
+import { Edit2, X } from "lucide-react";
 
 export type Contact = {
   name: string;
@@ -26,5 +28,20 @@ export const columns: ColumnDef<Contact>[] = [
   {
     accessorKey: "relationship",
     header: "Relationship",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <div className="flex flex-row gap-2">
+          <Button size="icon" variant="ghost">
+            <Edit2 />
+          </Button>
+          <Button size="icon" variant="destructive">
+            <X />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
