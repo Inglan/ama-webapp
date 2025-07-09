0ef7b1fa5a54e6ecb86bc578f41d0450bbb37fd3
Ingo Wolf <me@ingowolf.au>
2025-07-09 21:24:32 +1000

    add example datatable

--- app/(dashboard)/dashboard/contacts/page.tsx
+++ app/(dashboard)/dashboard/contacts/page.tsx
@@ -1,7 +1,15 @@
+import { columns, Payment } from "./columns";
+import { DataTable } from "./data-table";
+
 export default function ContactsPage() {
+  const data: Payment[] = [
+    { id: "728ed52f", amount: 100, status: "pending", email: "m@example.com" },
+  ];
+
   return (
-    <div className="max-w-2xl w-full mx-auto border border-dashed p-3 rounded-xl">
+    <div className="max-w-2xl w-full mx-auto p-3 gap-3 flex flex-col">
       <h1 className="text-3xl">Emergency Contacts</h1>
+      <DataTable data={data} columns={columns} />
     </div>
   );
 }
