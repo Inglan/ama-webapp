da3509f8fe633b593efee9a77682f34733548fda
Ingo Wolf <me@ingowolf.au>
2025-07-10 14:47:27 +1000

    add default role

--- convex/auth.ts
+++ convex/auth.ts
@@ -4,4 +4,13 @@ import Google from "@auth/core/providers/google";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [ResendOTP, Google],
+  callbacks: {
+    async afterUserCreatedOrUpdated(ctx, args) {
+      if (args.existingUserId) return;
+
+      await ctx.db.patch(args.userId, {
+        role: "parent",
+      });
+    },
+  },
 });
