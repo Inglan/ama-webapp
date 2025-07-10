4e8f07d6e2a18fa0249fbf132c72ea910c5ed83e
Ingo Wolf <me@ingowolf.au>
2025-07-10 16:12:57 +1000

    start adding form

--- app/(dashboard)/dashboard/contacts/columns.tsx
+++ app/(dashboard)/dashboard/contacts/columns.tsx
@@ -21,6 +21,20 @@ import { useState } from "react";
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 export type Contact = {
   id: string;
   name: string;
@@ -29,6 +43,21 @@ export type Contact = {
   altPhoneNumber?: string;
   relationship: string;
 };
+
+const updateFormSchema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  phoneNumber: z
+    .string()
+    .min(2)
+    .regex(/^[0-9]*$/),
+  altPhoneNumber: z
+    .string()
+    .min(2)
+    .regex(/^[0-9]*$/)
+    .optional(),
+  relationship: z.string().min(2),
+});
 
 export const columns: ColumnDef<Contact>[] = [
   {
@@ -53,6 +82,28 @@ export const columns: ColumnDef<Contact>[] = [
       const remove = useMutation(api.contacts.remove);
       const update = useMutation(api.contacts.update);
 
+      const updateForm = useForm<z.infer<typeof updateFormSchema>>({
+        resolver: zodResolver(updateFormSchema),
+        defaultValues: {
+          name: row.original.name,
+          email: row.original.email,
+          phoneNumber: row.original.phoneNumber,
+          altPhoneNumber: row.original.altPhoneNumber,
+          relationship: row.original.relationship,
+        },
+      });
+
+      function onUpdateFormSubmit(values: z.infer<typeof updateFormSchema>) {
+        update({
+          id: row.original.id as Id<"contacts">,
+          email: values.email,
+          name: values.name,
+          phoneNumber: values.phoneNumber,
+          altPhoneNumber: values.altPhoneNumber,
+          relationship: values.relationship,
+        });
+      }
+
       return (
         <div className="flex flex-row gap-2 justify-end">
           <Dialog>
@@ -64,74 +115,95 @@ export const columns: ColumnDef<Contact>[] = [
             >
               <Edit2 />
             </DialogTrigger>
-            <form
-              onSubmit={() => {
-                alert();
-              }}
-            >
-              <DialogContent>
-                <DialogTitle>Edit</DialogTitle>
-                <div className="grid w-full items-center gap-3">
-                  <Label htmlFor="name">Name</Label>
-                  <Input
-                    id="name"
-                    placeholder="Name"
-                    type="text"
-                    defaultValue={row.original.name}
-                  />
-                </div>
+            <DialogContent>
+              <DialogTitle>Edit</DialogTitle>
 
-                <div className="grid w-full items-center gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    placeholder="Email"
-                    type="email"
-                    defaultValue={row.original.email}
+              <Form {...updateForm}>
+                <form
+                  onSubmit={updateForm.handleSubmit(onUpdateFormSubmit)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={updateForm.control}
+                    name="name"
+                    render={() => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                </div>
 
-                <div className="grid w-full items-center gap-3">
-                  <Label htmlFor="phoneNumber">Phone Number</Label>
-                  <Input
-                    id="phoneNumber"
-                    placeholder="Phone Number"
-                    type="tel"
-                    defaultValue={row.original.phoneNumber}
+                  <FormField
+                    control={updateForm.control}
+                    name="phoneNumber"
+                    render={() => (
+                      <FormItem>
+                        <FormLabel>Phone</FormLabel>
+                        <FormControl>
+                          <Input />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                </div>
 
-                <div className="grid w-full items-center gap-3">
-                  <Label htmlFor="altPhoneNumber">Alternate Phone Number</Label>
-                  <Input
-                    id="altPhoneNumber"
-                    placeholder="Alternate Phone Number"
-                    type="tel"
-                    defaultValue={row.original.altPhoneNumber}
+                  <FormField
+                    control={updateForm.control}
+                    name="altPhoneNumber"
+                    render={() => (
+                      <FormItem>
+                        <FormLabel>Alternate Phone</FormLabel>
+                        <FormControl>
+                          <Input />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                </div>
-                <div className="grid w-full items-center gap-3">
-                  <Label htmlFor="relationship">Relationship</Label>
-                  <Input
-                    id="relationship"
-                    placeholder="Relationship"
-                    type="text"
-                    defaultValue={row.original.relationship}
-                  />
-                </div>
 
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button variant="outline">Cancel</Button>
-                  </DialogClose>
-                  <DialogClose asChild>
+                  <FormField
+                    control={updateForm.control}
+                    name="email"
+                    render={() => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={updateForm.control}
+                    name="relationship"
+                    render={() => (
+                      <FormItem>
+                        <FormLabel>Relationship</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Relationship" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DialogClose>
                     <Button variant="default" type="submit">
                       Save
                     </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </form>
+                  </DialogFooter>
+                </form>
+              </Form>
+            </DialogContent>
           </Dialog>
           <Button size="icon" variant="ghost">
             <Edit2 />
