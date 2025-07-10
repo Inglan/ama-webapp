4f879ce9b3982cb999d05b03a0d6d6ea4bd57130
Ingo Wolf <me@ingowolf.au>
2025-07-10 15:29:46 +1000

    add delete dialog

--- app/(dashboard)/dashboard/contacts/columns.tsx
+++ app/(dashboard)/dashboard/contacts/columns.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { ColumnDef } from "@tanstack/react-table";
 import { useMutation } from "convex/react";
 import { Edit2, X } from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export type Contact = {
   id: string;
@@ -42,13 +52,40 @@ export const columns: ColumnDef<Contact>[] = [
           <Button size="icon" variant="ghost">
             <Edit2 />
           </Button>
-          <Button
-            size="icon"
-            variant="destructive"
-            onClick={() => remove({ id: row.original.id as Id<"contacts"> })}
-          >
-            <X />
-          </Button>
+          <Dialog>
+            <DialogTrigger
+              className={buttonVariants({
+                variant: "destructive",
+                size: "icon",
+              })}
+            >
+              <X />
+            </DialogTrigger>
+
+            <DialogContent>
+              <DialogTitle>
+                Are you sure you would like to delete "{row.original.name}"?
+              </DialogTitle>
+              <DialogDescription>
+                This action cannot be undone.
+              </DialogDescription>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Button
+                    variant="destructive"
+                    onClick={() =>
+                      remove({ id: row.original.id as Id<"contacts"> })
+                    }
+                  >
+                    Delete
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       );
     },
