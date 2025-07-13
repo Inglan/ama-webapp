731ddf38e4f65e33634ca5a98b4c0113c4702ce0
Ingo Wolf <me@ingowolf.au>
2025-07-13 17:24:53 +1000

    add onboarding dialog

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,11 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { DashboardSidebar } from "@/components/dashboard-sidebar";
+import Onboarding from "@/components/onboarding";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
+      <Onboarding />
       <DashboardSidebar />
       <main className="w-full">
         <SidebarTrigger />
